cf5b54669501df13a81b3f4c48c025ce320e9c01
SergeySQ <sergey.sudarev2016@yandex.kz>
2020-08-08 00:52:09 +0600

    difficul task

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+let a = 266219 ;
+let sum = a.toString().split("").reduce(function(a,b){
+    return  a * b;
+    
+});
+console.log(sum = sum ** 3);
+alert(String(sum).slice(0,2));
+
