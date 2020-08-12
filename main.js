e04086790da8a436b9cd03ee7590671636a4113f
SergeySQ <sergey.sudarev2016@yandex.kz>
2020-08-13 03:30:56 +0600

    массив и простые числа

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-"use strict";
+//1задание
+let arr = [
+	"321",
+	"22213",
+	"213213",
+	"2131231",
+	"32131",
+	"45435345",
+	"12321312",
+];
 
-function arg(str) {
-    str = "hi";
-    if (typeof str !== "string") {
-        alert("Выввели не строку");
-        return;
-    }
-
-    str = str.trim();
-    return str.length > 30 ? str.slice(0, 30) + "..." : str;
+for (let i = 0; i < arr.length; i++) {
+	let a = arr[i];
+	if (a.startsWith("2") || a.startsWith("4")) {
+		console.log(a);
+	}
 }
-console.log(arg());
+//2задание
+let n = 100;
+for (let i = 2; i <= n; i++) {
+	let a = 1;
+	for (let j = 2; j <= i / 2 && a === 1; j = j + 1) {
+		if (i % j === 0) {
+			a = 0;
+		}
+	}
+
+	if (a === 1) {
+		console.log(i + ": делители этого числа: " + 1 + ", " + i);
+	}
+}
